e09f4348d2bf33b3dfa10d7da31e128e548a27d6
asteve12335 <asteve12335@gmail.com>
2022-11-22 17:35:04 +0000

    Slight adjustments

--- _printf.c
+++ _printf.c
@@ -13,7 +13,17 @@ int _printf(const char *format, ...)
 	conver_t flist[] = {
 		{"c", print_char},
 		{"s", print_string},
-		{"%", print_percent}
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
 	};
 	va_list arg_list;
 
@@ -22,7 +32,7 @@ int _printf(const char *format, ...)
 
 	va_start(arg_list, format);
 	/*Calling parser function*/
-	printed_chars = parser(format, flist, arg_list);
+	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
 }
